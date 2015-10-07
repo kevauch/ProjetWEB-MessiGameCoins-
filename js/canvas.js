5bcd8b0ace856806f97ed6bff137f00f7db32954
Kévin AUCHOYBUR <ak900234@etu.unice.fr>
2015-10-07 14:42:32 +0200

    Jeu Version 2 Ajout de Bombe, supression du timer, perte de vie si bombe touche, modification interface acceuil

--- js/canvas.js
+++ js/canvas.js
@@ -4,15 +4,30 @@ var keys=[];
 var width,height,speed;
 width=500;
 height=500;
-speed=3;
-var score = 0;
+speed=10;
+var score = 5;
 var gameState="menu";
+
 
 var messiAnime = new Image();	
 messiAnime.src='img/messiAnime.png';
 
-var ball = new Image();
-ball.src="img/ball.png"
+var ballAnime = new Image();
+ballAnime.src="img/ball.png"
+
+var bombAnime = new Image();
+bombAnime.src ="img/bomb.png"
+
+var bombIcon = new Image();
+bombIcon.src="img/bombIcon.png"
+
+
+var touches = new Image();
+touches.src="img/touches.png"
+
+var coeurIcon = new Image();
+coeurIcon.src="img/coeur.png"
+
 
 
 var player = {
@@ -27,13 +42,115 @@ var player = {
 };
 
 
-var cube ={
+var ball ={
 
 	x:Math.random()*(width-20),
 	y:Math.random()*(width-20),
 	width:30,
 	height:30
 };
+
+/*7 Bombes dans le jeu*/
+
+var bomb={
+
+	x:Math.random()*(width-40),
+	y:Math.random()*(width-40),
+	width:30,
+	height:30
+
+
+}
+
+var bomb1={
+
+	x:150,
+	y:150,
+	width:30,
+	height:30
+
+}
+
+var bomb2={
+
+	x:200,
+	y:350,
+	width:30,
+	height:30
+
+}
+
+var bomb3={
+
+	x:400,
+	y:350,
+	width:30,
+	height:30
+
+}
+
+var bomb4={
+
+	x:440,
+	y:190,
+	width:30,
+	height:30
+
+}
+
+var bomb5={
+
+	x:100,
+	y:350,
+	width:30,
+	height:30
+
+}
+
+var bomb6={
+
+	x:80,
+	y:120,
+	width:30,
+	height:30
+
+}
+
+var bomb7={
+
+	x:130,
+	y:240,
+	width:30,
+	height:30
+
+}
+
+var bomb8={
+
+	x:60,
+	y:150,
+	width:30,
+	height:30
+
+}
+
+var bomb9={
+
+	x:300,
+	y:200,
+	width:30,
+	height:30
+
+}
+
+var bomb10={
+
+	x:80,
+	y:150,
+	width:30,
+	height:30
+
+}
 
 window.addEventListener("keydown",function(e){
 
@@ -50,7 +167,7 @@ function game(){
 
 	update();
 	render();
-	gameOver();
+	
 	
 
 }
@@ -69,39 +186,134 @@ function render(){
 	ctx.drawImage(messiAnime,player.x,player.y,player.width,player.height);
 	
 
-	ctx.fillStyle="yellow";
-	/*ctx.fillRect(cube.x,cube.y,cube.width,cube.height);*/
-	ctx.drawImage(ball,cube.x,cube.y,cube.width,cube.height);
+	/*ctx.fillStyle="yellow";*/
+	/*ctx.fillRect(ball.x,ball.y,ball.width,ball.height);*/
+	ctx.drawImage(ballAnime,ball.x,ball.y,ball.width,ball.height);
+
+	ctx.drawImage(bombAnime,bomb.x,bomb.y,bomb.width,bomb.height);
+
+
+	ctx.drawImage(bombAnime,bomb1.x,bomb1.y,bomb1.width,bomb1.height);
+	ctx.drawImage(bombAnime,bomb2.x,bomb2.y,bomb2.width,bomb2.height);
+	ctx.drawImage(bombAnime,bomb3.x,bomb3.y,bomb3.width,bomb3.height);
+	ctx.drawImage(bombAnime,bomb4.x,bomb4.y,bomb4.width,bomb4.height);
+	ctx.drawImage(bombAnime,bomb5.x,bomb5.y,bomb5.width,bomb5.height);
+	ctx.drawImage(bombAnime,bomb6.x,bomb6.y,bomb6.width,bomb6.height);
+	ctx.drawImage(bombAnime,bomb7.x,bomb7.y,bomb7.width,bomb7.height);
+	ctx.drawImage(bombAnime,bomb8.x,bomb8.y,bomb8.width,bomb8.height);
+	ctx.drawImage(bombAnime,bomb9.x,bomb9.y,bomb9.width,bomb9.height);
+	ctx.drawImage(bombAnime,bomb10.x,bomb10.y,bomb10.width,bomb10.height);
 
 	ctx.fillStyle="white";
 	ctx.font="bold 20px helvetica";
-	ctx.fillText("Score : "+score,200,23);
+	ctx.fillText("Vies: "+score,200,23);
 
 	}
 
 
 	if(gameState === "menu"){
 
-		
+		ctx.save();
 		ctx.clearRect(0,0,width,height);
+		clearInterval(game);
 	  	ctx.font = "bold 50px consolas";
-        ctx.fillStyle = "white";
-        ctx.fillText("Collect Coins", width/2-180, height/2);
-        ctx.font = "bold 30px consolas";
-        ctx.fillText("Press Space", width/2-90, height/2+30);
+        ctx.fillStyle = "black";
+        ctx.drawImage(messiAnime,10,198);
+        ctx.fillText("Messi Ball", 115,250);
+        
+        ctx.font = "bold 20px Segoe UI";
+        ctx.fillText("Appuyez sur Espace pour commencer", 110,290);
+
+        ctx.drawImage(touches,10,340);
+        ctx.font = "bold 12px Segoe UI";
+        ctx.fillText("Dirigez Leo Messi avec les touches directionnelles", 90,360);
+
+
+        ctx.drawImage(ballAnime,20,377);
+        ctx.font = "bold 12px Segoe UI";
+        ctx.fillText("Gagnez des vies en collectant les ballons", 90,400);
+
+        ctx.drawImage(bombIcon,20,417);
+        ctx.font = "bold 12px Segoe UI";
+        ctx.fillText("Attention ! Une vie en moins si vous touchez une bombe", 90,434);
+
+          ctx.drawImage(coeurIcon,25,460);
+        ctx.font = "bold 12px Segoe UI";
+        ctx.fillText("Disposez de 5 vies pour commencer", 90,470);
+        
         
         if(keys[32]) gameState = "play";
+
 	}
+
+   if (score<=0) {
+
+		ctx.save();
+		ctx.clearRect(0,0,width,height);
+		clearInterval(game);
+	  	ctx.font = "bold 50px consolas";
+        ctx.fillStyle = "black";
+        ctx.fillText("Game Over", 115, 250);
+        ctx.font = "bold 30px consolas";
+       
+        
+        if(keys[32]) gameState = "play";
+        }
+
 
 
 
 }
 
-function process(){
+function processBall(){
 
 	score++;
-	cube.x=Math.random()*(width-20);
-	cube.y=Math.random()*(width-20);
+
+	ball.x=Math.random()*(width-20);
+	ball.y=Math.random()*(width-20);
+	var audioCoin = new Audio('audio/coin.mp3');
+	audioCoin.play();
+}
+
+function processBomb(){
+
+	score --;
+	
+	bomb.x=Math.random()*(width-20);
+	bomb.y=Math.random()*(width-20);
+
+	bomb1.x=Math.random()*(width+20);
+	bomb1.y=Math.random()*(width+20);
+
+	bomb2.x=Math.random()*(width+20);
+	bomb2.y=Math.random()*(width+20);
+
+	bomb3.x=Math.random()*(width+20);
+	bomb3.y=Math.random()*(width+20);
+
+	bomb4.x=Math.random()*(width+20);
+	bomb4.y=Math.random()*(width+20);
+
+	bomb5.x=Math.random()*(width+20);
+	bomb5.y=Math.random()*(width+20);
+
+
+	bomb6.x=Math.random()*(width+20);
+	bomb6.y=Math.random()*(width+20);
+
+	bomb7.x=Math.random()*(width+20);
+	bomb7.y=Math.random()*(width+20);
+
+
+	bomb8.x=Math.random()*(width+20);
+	bomb8.y=Math.random()*(width+20);
+
+	bomb9.x=Math.random()*(width+20);
+	bomb9.y=Math.random()*(width+20);
+
+	bomb10.x=Math.random()*(width+20);
+	bomb10.y=Math.random()*(width+20);
+
 	var audioCoin = new Audio('audio/coin.mp3');
 	audioCoin.play();
 }
@@ -131,72 +343,25 @@ function update(){
 	if (player.y<0)player.x=0;
 	if (player.y>580)player.x=580;
 
-	if(collision(player,cube)) process();
+	if(collision(player,ball)) processBall();
+	
+	if(collision(player,bomb)) processBomb();
+	if(collision(player,bomb1)) processBomb();
+	if(collision(player,bomb2)) processBomb();
+	if(collision(player,bomb3)) processBomb();
+	if(collision(player,bomb4)) processBomb();
+	if(collision(player,bomb5)) processBomb();
+	if(collision(player,bomb6)) processBomb();
+	if(collision(player,bomb7)) processBomb();
+	if(collision(player,bomb8)) processBomb();
+	if(collision(player,bomb9)) processBomb();
+	if(collision(player,bomb10)) processBomb();
 
 
 }
 
- var startTime;
-
- function gameOver(){
-    var elapsed = parseInt((new Date() - startTime) / 1000);
-        ctx.save();
-        ctx.beginPath();
-        ctx.fillStyle="red";
-        ctx.font="20px Verdana"
-        // draw the running time at half opacity
-       // ctx.globalAlpha= 0.50;
-        ctx.fillText(elapsed+" secs",canvas.width-90,21);
-        ctx.restore();
-
-        if (elapsed> 30){
-        	 /* ctx.save();*/
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-     	  
-ctx.font = "20px Arial";
-        ctx.fillStyle = "Black";
-        ctx.fillText("Your score is"+ score,20,100);
-       //alert("Your score is"+score);
-       /*clearInterval(render);*/
-
-
-
-        }
-
-         if (elapsed> 30 && score<=18){
-        	 /* ctx.save();*/
-		
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-     	ctx.font = "20px Arial";
-        ctx.fillStyle = "Black";
-        ctx.fillText("Score de "+ score+" Niveau amateur",50,100);
-       /*clearInterval(render);*/
-      /* document.getElementById('song').stop();*/
-
-
-
-
-        }
-
-        if (elapsed> 30 && score>18){
-        	 /* ctx.save();*/
 
 	
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.font = "20px Arial";
-    ctx.fillStyle = "White";
-    ctx.fillText("Score de " + score+" Niveau professionnel",20,100);
-       /*clearInterval(render);*/
-
-
-
-        }
-
-
-        		/*var audioPlay = new Audio('audio/musicGame.mp3');
-				audioPlay.play();*/
- 
-    }
 
   /*function drawFinalScore() {
         // set the final score just once
@@ -213,7 +378,6 @@ setInterval(function(){
 	game();
 },1000/60)
 
- startTime = new Date();
 
 
 
